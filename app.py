47c4d5463d0ed42bafd3261ba8c8161e96ca0d42
mohd-saifuddin <mohd.saifu42@gmail.com>
2023-01-02 22:12:31 +0530

    Formatted the code

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import streamlit as st
+from mlpipeline import Pipline
 import warnings
 warnings.filterwarnings('ignore')
 
-from mlpipeline import Pipline
-import streamlit as st
 
 st.set_page_config(layout='wide')
 
@@ -14,20 +14,26 @@ st.markdown(
 col1, col2 = st.columns([1, 2])
 
 with col1:
-    alpha = 150 # It can be any random value between 0 to 360. Doesn't contribute to model prediction.
-    delta = 150 # It can be any random value between 0 to 360. Doesn't contribute to model prediction.
-    u = st.slider(label='Ultraviolet', min_value=0.0, max_value=30.0, value=22.0)
+    # It can be any random value between 0 to 360. Doesn't contribute to model prediction.
+    alpha = 150
+    # It can be any random value between 0 to 360. Doesn't contribute to model prediction.
+    delta = 150
+    u = st.slider(label='Ultraviolet', min_value=0.0,
+                  max_value=30.0, value=22.0)
     g = st.slider(label='Green', min_value=0.0, max_value=30.0, value=22.0)
     r = st.slider(label='Red', min_value=0.0, max_value=30.0, value=25.0)
-    i = st.slider(label='Infrared (I)', min_value=0.0, max_value=30.0, value=10.0)
-    z = st.slider(label='Infrared (Z)', min_value=0.0, max_value=30.0, value=5.0)
-    redshift = st.slider(label='Redshift', min_value=0.0, max_value=10.0, value=2.0)
+    i = st.slider(label='Infrared (I)', min_value=0.0,
+                  max_value=30.0, value=10.0)
+    z = st.slider(label='Infrared (Z)', min_value=0.0,
+                  max_value=30.0, value=5.0)
+    redshift = st.slider(label='Redshift', min_value=0.0,
+                         max_value=10.0, value=2.0)
 
 data = [[alpha, delta, u, g, r, i, z, redshift]]
 pipe = Pipline(data=data)
 conclusion, fig, pred_class = pipe.pipeline()
-image_credits = "A random {} image taken from nasa.gov image gallery.".format(pred_class.lower())
-
+image_credits = "A random {} image taken from nasa.gov image gallery.".format(
+    pred_class.lower())
 conclusion = "<p style='text-align: center;'>{}</p>".format(conclusion)
 image_credits = "<p style='text-align: center;'>{}</p>".format(image_credits)
 
--- mlpipeline.py
+++ mlpipeline.py
@@ -1,13 +1,13 @@
+import random
+import plotly.express as px
+import pickle
+import pandas as pd
+import os
+import numpy as np
+import cv2 as cv
 import warnings
 warnings.filterwarnings('ignore')
 
-import cv2 as cv
-import numpy as np
-import os
-import pandas as pd
-import pickle
-import plotly.express as px
-import random
 
 class Pipline(object):
     """
@@ -20,7 +20,7 @@ class Pipline(object):
         self.features = ['alpha', 'delta', 'u', 'g', 'r', 'i', 'z', 'redshift']
         self.target = 'class'
         self.df = None
-    
+
     def fetch(self) -> None:
         """
         This method fetches the data.
@@ -33,10 +33,10 @@ class Pipline(object):
         """
         with open(file='scaling.pkl', mode='rb') as s_pkl:
             scaling = pickle.load(file=s_pkl)
-        
+
         self.df = scaling.transform(X=self.df)
         self.df = pd.DataFrame(data=self.df, columns=self.features)
-    
+
     def featurize(self) -> None:
         """
         This method performs feature engineering on the data.
@@ -48,14 +48,14 @@ class Pipline(object):
         self.df['i-r'] = self.df['i'] - self.df['r']
         self.df['z-r'] = self.df['z'] - self.df['r']
         self.df = self.df[fi_cols]
-    
+
     def predict(self) -> tuple:
         """
         This method predicts the query datapoint.
         """
         with open(file='stacking_classifier.pkl', mode='rb') as m_pkl:
-            clf  = pickle.load(file=m_pkl)
-        
+            clf = pickle.load(file=m_pkl)
+
         pred_proba = clf.predict_proba(X=self.df)
         confidence = np.round(a=np.max(a=pred_proba)*100, decimals=2)
         pred_class = clf.predict(X=self.df)[0]
@@ -68,10 +68,11 @@ class Pipline(object):
             pred_class = 'Galaxy'
         else:
             pred_class = 'Star'
-        
-        c = "The class is '{}' with a confidence of {}%.".format(pred_class, confidence)
+
+        c = "The class is '{}' with a confidence of {}%.".format(
+            pred_class, confidence)
         return c, fig, pred_class
-    
+
     def get_encoded_image(self, pred_class):
         """
         This method selects an image and encodes it.
